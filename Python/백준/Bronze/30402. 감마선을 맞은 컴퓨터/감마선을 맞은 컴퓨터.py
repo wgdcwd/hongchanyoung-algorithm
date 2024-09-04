56a7113864216568f5cd4bfc9fd6cb46f79e591e
wgdcwd <86083157+wgdcwd@users.noreply.github.com>
2024-09-04 12:30:51 +0900

    [Bronze IV] Title: 감마선을 맞은 컴퓨터, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

---
+++ Python/백준/Bronze/30402. 감마선을 맞은 컴퓨터/감마선을 맞은 컴퓨터.py
@@ -0,0 +1,12 @@
+arr = [list(input()) for _ in range(15)]
+for i in arr:
+    for j in i:
+        if j == 'w':
+            print("chunbae")
+            exit(0)
+        elif j == 'b':
+            print("nabi")
+            exit(0)
+        elif j == 'g':
+            print("yeongcheol")
+            exit(0)
